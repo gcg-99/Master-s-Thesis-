872eef84d118f0893fee994b8c02d1a30a9484cb
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-04-30 19:45:10 +0200

    Update input_gross.sh

--- Outlier_test/input_gross.sh
+++ Outlier_test/input_gross.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tabix -h hgdp_tgp_sgdp_chr12_p.dated.vcf.gz  | glactools vcfm2acf --onlyGT --fai human_g1k_v37.fasta.fai  -  | glactools meld -f panel.txt -  | glactools acf2gross --noroot - | gzip > input.gross.gz
+tabix -h hgdp_tgp_sgdp_chr12_p.dated.vcf.gz  | glactools vcfm2acf --onlyGT --fai human_g1k_v37.fa.fai  -  | glactools meld -f panel.txt -  | glactools acf2gross --noroot - | gzip > input.gross.gz
