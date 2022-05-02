0aec0f38b5e00c359f650d517770bf06e37fb5cb
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-05-02 11:32:33 +0200

    Update GRoSS.sh

--- Outlier_test/GRoSS.sh
+++ Outlier_test/GRoSS.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-Rscript GRoSS.R -e $input -r $graph -o chr12_0.05.tsv
+# run GRoSS with the phylogenetic tree topology obtained with Treemix
+Rscript GRoSS.R -e $input -r chr12_0.05.graph -o chr12_0.05.tsv
+
+# run GRoSS with the phylogenetic tree topology built with an expert help
+Rscript GRoSS.R -e $input -r chr12_0.05_2.graph -o chr12_0.05_2.tsv
