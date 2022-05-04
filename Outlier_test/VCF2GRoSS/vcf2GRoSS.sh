7c546666c7860374f8656cde7e3b83d0cde59ecb
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-05-04 18:06:29 +0200

    Update and rename Outlier_test/vcf2GRoSS_2.sh to Outlier_test/VCF2GRoSS/vcf2GRoSS.sh

---
+++ Outlier_test/VCF2GRoSS/vcf2GRoSS.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./glactools/glactools vcfm2acf --onlyGT --fai human_g1k_v37.fasta.fai hgdp_tgp_sgdp_chr12_p.dated_287.vcf | ./glactools/glactools meld -f panel.txt - | ./glactools/glactools acf2gross --noroot - | gzip > $out/"human_chr12.gross.gz"
--- Outlier_test/vcf2GRoSS_2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-glactools="/media/worky_ant/Seagate_Backup_Plus_Drive/glactools"
-input="/media/worky_ant/Seagate_Backup_Plus_Drive/AntropoGeoSNPs/00_input/Database/hgdp_tgp_sgdp_chr12_p.dated_287.vcf"
-index="/media/worky_ant/Seagate_Backup_Plus_Drive/AntropoGeoSNPs/00_input/reference/human_g1k_v37.fasta.fai"
-panel="/media/worky_ant/Seagate_Backup_Plus_Drive/AntropoGeoSNPs/00_input/Database/panel.txt"
-out="/media/worky_ant/Seagate_Backup_Plus_Drive/AntropoGeoSNPs/00_input/GRoSS"
-
-$glactools/glactools vcfm2acf --onlyGT --fai $index $input | $glactools/glactools meld -f $panel - | $glactools/glactools acf2gross --noroot - | gzip > $out/"human_chr12.gross.gz"
