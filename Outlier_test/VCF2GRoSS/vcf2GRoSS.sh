65bbe0fc6cb3a232725227726ff5e5addc36cf69
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-05-04 18:11:14 +0200

    Update vcf2GRoSS.sh

--- Outlier_test/VCF2GRoSS/vcf2GRoSS.sh
+++ Outlier_test/VCF2GRoSS/vcf2GRoSS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-output=""
+output="Master-s-Thesis-/Outlier_test/"
 
-./glactools/glactools vcfm2acf --onlyGT --fai human_g1k_v37.fasta.fai hgdp_tgp_sgdp_chr12_p.dated_287.vcf | ./glactools/glactools meld -f panel.txt - | ./glactools/glactools acf2gross --noroot - | gzip > $out/"human_chr12.gross.gz"
+./glactools/glactools vcfm2acf --onlyGT --fai human_g1k_v37.fasta.fai hgdp_tgp_sgdp_chr12_p.dated_287.vcf | ./glactools/glactools meld -f panel.txt - | ./glactools/glactools acf2gross --noroot - | gzip > $output/"human_chr12.gross.gz"
