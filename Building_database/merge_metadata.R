4d12d04afb3acfba6acca619de9a18a24a62aadb
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-05-04 16:08:28 +0200

    Update merge_metadata.R

--- Building_database/merge_metadata.R
+++ Building_database/merge_metadata.R
@@ -1,10 +1,8 @@
-R
+#!/usr/bin/env Rscript
+
 all <- read.csv("ind_list.csv")
 samples <- read.csv("samples.csv", sep = "\t")
 
 merged <- merge(all, samples, by.x = "FINAL")
 
 write.csv(merged, "merged.csv")
-	
-# Here I realized some of our samples are not in the unified vcf, I wrote those in a new
-# sheet within "Base datos definitiva.xlsx" called "Faltan".
