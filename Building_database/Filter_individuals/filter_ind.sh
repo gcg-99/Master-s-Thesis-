79c12b7bd32b356a6c7d8a625a42989a6570a157
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-05-04 17:29:22 +0200

    Update filter_ind.sh

--- Building_database/Filter_individuals/filter_ind.sh
+++ Building_database/Filter_individuals/filter_ind.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-vcftools --vcf hgdp_tgp_sgdp_chr12_p.dated.vcf --keep samples --recode --stdout | bgzip -c > hgdp_tgp_sgdp_chr12_p.dated.vcf.gz
+vcftools --vcf hgdp_tgp_sgdp_chr12_p.dated.vcf --keep samples.txt --recode --stdout | bgzip -c > hgdp_tgp_sgdp_chr12_p.dated_287.vcf.gz
 # This is our final vcf
