c06ccee5955c7d53bb80c4fe585643ed63aff499
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-05-04 18:29:00 +0200

    Update GRoSS.sh

--- Outlier_test/GRoSS.sh
+++ Outlier_test/GRoSS.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# run GRoSS with the phylogenetic tree topology obtained with Treemix
-Rscript GRoSS.R -e $input -r chr12_0.05.graph -o chr12_0.05.tsv
+gross_dir="/media/worky_ant/Seagate_Backup_Plus_Drive/AntropoGeoSNPs/00_input/GRoSS"
+input="$gross_dir/human_chr12.gross"
+output="$gross_dir/output_GRoSS"
 
-# run GRoSS with the phylogenetic tree topology built with an expert help
-Rscript GRoSS.R -e $input -r chr12_0.05_2.graph -o chr12_0.05_2.tsv
+
+# for Treemix topology
+Rscript GRoSS.R -e human_chr12.gross -r human_chr12_1.graph -o $output/"human_chr12_1.tsv"
+
+# for our topology
+Rscript GRoSS.R -e human_chr12.gross -r human_chr12_2.graph -o $output/"human_chr12_2.tsv"
