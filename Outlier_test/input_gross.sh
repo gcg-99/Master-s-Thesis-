7fc19b93c52db9ce50d20f85746c824f94cbbefb
Alicia Portela <alicia.portela@alumnos.upm.es>
2022-05-04 18:03:13 +0200

    Delete input_gross.sh

--- Outlier_test/input_gross.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tabix -h hgdp_tgp_sgdp_chr12_p.dated.vcf.gz  | glactools vcfm2acf --onlyGT --fai human_g1k_v37.fa.fai  -  | glactools meld -f panel.txt -  | glactools acf2gross --noroot - | gzip > input.gross.gz
